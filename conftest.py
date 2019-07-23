0552294b391722e15dfb597c23c150ceda7053f4
marinayanga <marinayanga@gmail.com>
2019-07-23 13:27:34 +0300

    Доавлен тест на создание группы

--- conftest.py
+++ conftest.py
@@ -4,6 +4,6 @@ from fixture.application import Application
 
 @pytest.fixture(scope="session")
 def app(request):
-    fixture=Application("c:\\PycharmProjects\\AddressBook-gui\\AddressBook.exe")
+    fixture=Application("C:\\Users\\kokorevama\\Desktop\\Папка в папке\\FreeAddressBookPortable\\AddressBook.exe")
     request.addfinalizer(fixture.destroy)
     return fixture
