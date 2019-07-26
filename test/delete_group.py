4eb1fcf7819551ba86f78f667b11a5d6fb69775b
marinayanga <marinayanga@gmail.com>
2019-07-26 12:52:16 +0300

    23/ UPD Cоздать тест на удаление группы

--- test/delete_group.py
+++ test/delete_group.py
@@ -2,10 +2,12 @@ from random import randrange
 
 
 def test_delete_group(app):
-    if len (app.group.get_group_list()) == 0:
+    if len (app.group.get_group_list()) <= 1:
          app.group.add_new_group("my group")
     old_list = app.group.get_group_list()
     index = randrange(len(old_list))
     app.group.delete_group(index)
+    del old_list[index]
     new_list = app.group.get_group_list()
-    assert len(old_list)-1 == len(new_list)
+    assert len(old_list) == len(new_list)
+    assert sorted(old_list) == sorted(new_list)
